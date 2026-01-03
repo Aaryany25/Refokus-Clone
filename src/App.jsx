c09a4b56ca06a5d172a3f2e301e91ed5c030e3f1
Aaryan <aaryany2505@gmail.com>
2026-01-04 00:27:05 +0530

    Started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className='bg-zinc-900 text-white w-full h-screen'>
+   <Navbar/>
+    </div>
+  )
+}
+
+export default App
