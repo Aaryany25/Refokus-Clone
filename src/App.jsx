226e90a1bb83c75f7ee4ba0e50673a88f7c7cf62
Aaryan <aaryany2505@gmail.com>
2026-01-07 12:05:25 +0530

    Added Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Stripes from './components/Stripes'
 import Products from './components/Products'
 import Marquees from './components/Marquees'
 import Cards from './components/Cards'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div className='bg-zinc-900 text-white w-full '>
@@ -14,6 +16,7 @@ function App() {
    <Products/>
    <Marquees/>
    <Cards/>
+   <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full'>
+      <div className='max-w-screen-xl mx-auto p-10 flex gap-10'>
+        <div className='basis-1/2'>
+<h1 className='text-9xl font-bold'>Refokus.</h1>
+        </div>
+        <div className='basis-1/2'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
